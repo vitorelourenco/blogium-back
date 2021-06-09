e313ff9ef0384bad9fceda62d4866fdf7ef73d57
Vitor Lourenco <vitor.registros@lourencos.net>
2021-06-09 19:35:37 -0300

    Implement error handling

--- app.js
+++ app.js
@@ -28,32 +28,44 @@ app.get("/posts", (req,res)=>{
 
 app.post("/posts", (req,res)=>{
   const post = req.body;
-  post.contentPreview = post.content.substring(0,50);
-  post.commentCount = 0;
-  post.id = nextPostId;
-  nextPostId++;
-  posts.push(post);
-  res.send("OK");
+  if (!post.content) res.status(400).send('Content missing');
+  else if (!post.coverUrl) res.status(400).send('Image URL missing');
+  else if (!post.title) res.status(400).send('title missing');
+  else {
+    post.contentPreview = post.content.substring(0,50);
+    post.commentCount = 0;
+    post.id = nextPostId;
+    nextPostId++;
+    posts.push(post);
+    res.send("OK");
+  }
 });
 
 app.get("/posts/:id", (req,res)=>{
   const id = req.params.id;
   const post = posts.find(elem => elem.id.toString() === id);
   if (post) res.send(post);
-  else res.send("ERR");
+  else res.status(400).send('Post not found');
 });
 
 app.put("/posts/:id", (req,res)=>{
   const id = req.params.id;
   const post = posts.find(elem => elem.id.toString() === id);
+
   if (post) {
-    post.coverUrl = req.body.coverUrl;
-    post.content = req.body.content;
-    post.title = req.body.title;
-    post.contentPreview = post.title.substring(0,50);
-    res.send("OK");
+    const body = req.body;
+    if (!body.content) res.status(400).send('Content missing');
+    else if (!body.coverUrl) res.status(400).send('Image URL missing');
+    else if (!body.title) res.status(400).send('title missing');
+    else {
+      post.coverUrl = body.coverUrl;
+      post.content = body.content;
+      post.title = body.title;
+      post.contentPreview = post.title.substring(0,50);
+      res.send("OK");
+    }
   }
-  else res.send("ERR");
+  else res.status(400).send('Post not found');
 });
 
 app.delete("/posts/:id", (req,res)=>{
@@ -69,7 +81,7 @@ app.delete("/posts/:id", (req,res)=>{
   if (found) {
     res.send("OK");
   }
-  else res.send("ERR");
+  else res.status(400).send('Post not found');;
 });
 
 app.get("/posts/:id/comments", (req,res)=>{
@@ -81,16 +93,20 @@ app.get("/posts/:id/comments", (req,res)=>{
 app.post("/posts/:id/comments", (req,res)=>{
   const id = req.params.id;
   const comment = req.body;
-  comment.postId = parseInt(id);
-  comment.id = nextCommentId;
-  nextCommentId++;
-  comments.push(comment);
-  res.send("OK");
+  if (!comment.author) res.status(400).send('Author is missing');
+  else if (!comment.content) res.status(400).send('Content is missing');
+  else {
+    comment.postId = parseInt(id);
+    comment.id = nextCommentId;
+    nextCommentId++;
+    comments.push(comment);
+    res.send("OK");
+  }
 });
 
 app.listen(4000);
 
 process.on('exit', ()=>{
   writeFileSync('./posts.json', JSON.stringify({posts, nextPostId}));
-  writeFileSync('./comments.json', JSON.stringify({comments,nextCommentId}));
+  writeFileSync('./comments.json', JSON.stringify({comments, nextCommentId}));
 })
