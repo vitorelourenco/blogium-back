92d6fc007df9c692ec09448efed2bcd3b68fb3ea
Vitor Lourenco <vitor.registros@lourencos.net>
2021-06-10 11:04:00 -0300

    Fix persistent storage bug

--- app.js
+++ app.js
@@ -6,27 +6,39 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-if (!existsSync('./posts.json')){
-  writeFileSync('./posts.json', JSON.stringify({posts:[], nextPostId:1}));
+function getComments(){
+  const commentStorage = JSON.parse(readFileSync('./comments.json'));
+  const comments = commentStorage.comments;
+  const nextCommentId = commentStorage.nextCommentId;
+  return ({comments, nextCommentId});
 }
-const postStorage = JSON.parse(readFileSync('./posts.json'));
 
-if (!existsSync('./comments.json')){
-  writeFileSync('./comments.json', JSON.stringify({comments:[],nextCommentId:1}));
+function setComments(comments, nextCommentId){
+  writeFileSync('./comments.json', JSON.stringify({comments,nextCommentId}));
 }
-const commentStorage = JSON.parse(readFileSync('./comments.json'));
 
-const posts = postStorage.posts;
-let nextPostId = postStorage.nextPostId;
+function getPosts(){
+  const postStorage = JSON.parse(readFileSync('./posts.json'));
+  const posts = postStorage.posts;
+  const nextPostId = postStorage.nextPostId;
+  return ({posts, nextPostId});
+}
 
-const comments = commentStorage.comments;
-let nextCommentId = commentStorage.nextCommentId;
+function setPosts(posts, nextPostId){
+  writeFileSync('./posts.json', JSON.stringify({posts, nextPostId}));
+}
+
+if (!existsSync('./posts.json')) setPosts({posts:[], nextPostId:1});
+if (!existsSync('./comments.json')) setComments({comments:[],nextCommentId:1});
 
 app.get("/posts", (req,res)=>{
+  const {posts} = getPosts();
+  console.log(posts);
   res.send(posts);
 });
 
 app.post("/posts", (req,res)=>{
+  const {posts, nextPostId} = getPosts();
   const post = req.body;
   if (!post.content) res.status(400).send('Content missing');
   else if (!post.coverUrl) res.status(400).send('Image URL missing');
@@ -35,13 +47,14 @@ app.post("/posts", (req,res)=>{
     post.contentPreview = post.content.substring(0,50);
     post.commentCount = 0;
     post.id = nextPostId;
-    nextPostId++;
     posts.push(post);
     res.send("OK");
+    setPosts(posts, nextPostId+1)
   }
 });
 
 app.get("/posts/:id", (req,res)=>{
+  const {posts} = getPosts();
   const id = req.params.id;
   const post = posts.find(elem => elem.id.toString() === id);
   if (post) res.send(post);
@@ -49,6 +62,7 @@ app.get("/posts/:id", (req,res)=>{
 });
 
 app.put("/posts/:id", (req,res)=>{
+  const {posts, nextPostId} = getPosts();
   const id = req.params.id;
   const post = posts.find(elem => elem.id.toString() === id);
 
@@ -63,12 +77,15 @@ app.put("/posts/:id", (req,res)=>{
       post.title = body.title;
       post.contentPreview = post.title.substring(0,50);
       res.send("OK");
+      setPosts(posts, nextPostId);
     }
   }
   else res.status(400).send('Post not found');
 });
 
 app.delete("/posts/:id", (req,res)=>{
+  const {posts, nextPostId} = getPosts();
+
   const id = req.params.id;
   let found = false;
   for (let i=0; i<posts.length; i++){
@@ -80,17 +97,22 @@ app.delete("/posts/:id", (req,res)=>{
   }
   if (found) {
     res.send("OK");
+    setPosts(posts, nextPostId);
   }
   else res.status(400).send('Post not found');;
 });
 
 app.get("/posts/:id/comments", (req,res)=>{
+  const {comments} = getComments();
   const id = req.params.id;
   const postComments = comments.filter(elem=>elem.postId.toString() === id);
   res.send(postComments);
 });
 
 app.post("/posts/:id/comments", (req,res)=>{
+  const {posts, nextPostId} = getPosts();
+  const {comments, nextCommentId} = getComments();
+
   const id = req.params.id;
   const comment = req.body;
   if (!comment.author) res.status(400).send('Author is missing');
@@ -98,8 +120,13 @@ app.post("/posts/:id/comments", (req,res)=>{
   else {
     comment.postId = parseInt(id);
     comment.id = nextCommentId;
-    nextCommentId++;
     comments.push(comment);
+    
+    const post = posts.find(elem => elem.id.toString() === id);
+    post.commentCount++;
+
+    setPosts(posts, nextPostId);
+    setComments(comments, nextCommentId+1)
     res.send("OK");
   }
 });
